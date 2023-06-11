22edd4dd01b147a5c7ff38619f7c637c8da579c3
Kuba <kubamalewicz002@gmail.com>
2023-06-11 11:22:42 +0200

    Dokończona funckja update

--- main.py
+++ main.py
@@ -15,3 +15,21 @@ def update(screen, cells, size, with_progress=False):
     for row, col in np.ndindex(cells.shape):
         alive = np.sum(cells[row - 1:row + 2, col - 1:col + 2]) - cells[row, col]
         color = COLOR_BG if cells[row, col] == 0 else COLOR_ALIVE_NEXT
+
+        if cells[row, col] == 1:
+            if alive < 2 or alive > 3:
+                if with_progress:
+                    color = COLOR_DIE_NEXT
+            elif 2 <= alive <= 3:
+                update_cells[row, col] = 1
+                if with_progress:
+                    color = COLOR_ALIVE_NEXT
+        else:
+            if alive == 3:
+                update_cells[row, col] = 1
+                if with_progress:
+                    color = COLOR_ALIVE_NEXT
+
+        pygame.draw.rect(screen, color, (col * size, row * size, size - 1, size - 1))
+
+    return update_cells
