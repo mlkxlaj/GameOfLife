f945a9b5710ba38ce34f354bc9b96dcabb57cc1b
Mikołaj Kowaszewicz <s24427@pjwstk.edu.pl>
2023-06-19 16:12:38 +0200

    Wygenerowanie pliku html

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -19,7 +19,7 @@ release = '1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['rinoh.frontend.sphinx','sphinx.ext.autodoc','sphinx.ext.napoleon']
+
 
 templates_path = ['_templates']
 exclude_patterns = []
