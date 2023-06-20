7dd4bfec987f1a0227ae334c647ab9cecf0fdbbc
Mikołaj Kowaszewicz <s24427@pjwstk.edu.pl>
2023-06-20 19:01:06 +0200

    Dokumentacja skonczona

--- docs/conf.py
+++ docs/conf.py
@@ -10,6 +10,9 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, os.path.abspath("../Project"))
+
+
 
 project = 'Game Of Life'
 copyright = '2023, Kuba Malewicz, Mikolaj Kowaszewicz'
